e8f73b5cb1099f6bbef986d2cd7438a32b880cda
DanMaidana <danilomaidana.a@gmail.com>
2020-11-18 13:23:08 -0300

    hice cambios en HomePage

--- src/homePage.js
+++ src/homePage.js
@@ -1,8 +1,10 @@
 const fs = require('fs')
 
 module.exports = {
+    titulo :'Bienvenidos a DH Movies el mejor sitio para encontrar las mejores películas, incluso mucho mejor que Netflix, Cuevana y PopCorn​ .',
     bd : './data/movies.json',
     leerJSON : function(){
         return JSON.parse(fs.readFileSync(this.bd,'utf-8'));
     }
+
 }
